5c7243b47a5bcefedbeda19b5390e94f83510ae2
ypy <yuxiang_163com@163.com>
2023-06-12 18:28:54 +0800

    modify package_vimrc_ye_cfg

--- package_vimrc_ye_cfg.sh
+++ package_vimrc_ye_cfg.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -x
 
-cp -f .bashrc bashrc
-cp -f .vimrc vimrc
-rm -f vimrc_ye_cfg.zip
-zip -r vimrc_ye_cfg.zip bashrc vimrc .vim package_vimrc_ye_cfg.sh
-rm -f bashrc vimrc
+cp -f ~/.bashrc bashrc
+cp -f ~/.vimrc vimrc-ex
+rm -f ~/vimrc_ye_cfg.zip
+zip -r vimrc_ye_cfg.zip bashrc vimrc-ex .vim package_vimrc_ye_cfg.sh
